10966d86a888fe592c8b912a7265c2a32f8ba408
Joseph J <jangajoseph77@gmail.com>
2026-01-30 10:36:20 +0530

    Stylings

--- app/App.js
+++ app/App.js
@@ -2,24 +2,49 @@ import React from "react";
 import { View, Text, Button, TouchableOpacity, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { StatusBar } from "react-native";
+
 const App = () => {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View>
-        <Text style={Mystyle.mytext}>Hii my name is joseph </Text>
-      </View>
+	return (
+		<>
+			<StatusBar barStyle="dark-content" ></StatusBar>
+ 			<View style={Mystyle.parent}>
+				<View style={Mystyle.child1} ></View>
+				<View style={Mystyle.child2} ></View>
+			</View>
 
-    </SafeAreaView>
+		</>
 
-  )
+	)
 }
-
 export default App;
 
 const Mystyle = StyleSheet.create({
-  mytext: {
-    color: "red",
-    fontSize: 100,
-    fontWeight:800,
-  }
+	// mytext: {
+	// 	color: "white",
+
+	// 	fontWeight: 10,
+	// },
+	parent: {
+
+		height: 500,
+		backgroundColor: "red",
+		width: "100%",
+		borderWidth: 2,
+	},
+
+	child1: {
+		height: 200,
+		backgroundColor: "yellow",
+		width: "50%",
+
+	},
+	child2: {
+		width: 20,
+		backgroundColor: "blue",
+		height: 40,
+	}
+
+
+
 })
