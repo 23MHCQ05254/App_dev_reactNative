1911a0913f7ee491ae24c61336cf251ed83ed8d9
Joseph J <jangajoseph77@gmail.com>
2026-02-10 10:23:07 +0530

    basic components styling

--- app/App.js
+++ app/App.js
@@ -1,50 +1,17 @@
-import React from "react";
-import { View, Text, Button, TouchableOpacity, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-
+import react from "react";
+import { View, Text, Button, TouchableOpacity, Image, StyleSheet } from "react-native";
+import MyImage from "./assets/favicon.png"
 import { StatusBar } from "react-native";
-
+import Component from "./Component";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 const App = () => {
-	return (
-		<>
-			<StatusBar barStyle="dark-content" ></StatusBar>
- 			<View style={Mystyle.parent}>
-				<View style={Mystyle.child1} ></View>
-				<View style={Mystyle.child2} ></View>
-			</View>
-
-		</>
-
-	)
+  return (
+    <>
+      <SafeAreaProvider>
+        <Component />
+      </SafeAreaProvider>
+    </>
+  )
 }
+
 export default App;
-
-const Mystyle = StyleSheet.create({
-	// mytext: {
-	// 	color: "white",
-
-	// 	fontWeight: 10,
-	// },
-	parent: {
-
-		height: 500,
-		backgroundColor: "red",
-		width: "100%",
-		borderWidth: 2,
-	},
-
-	child1: {
-		height: 200,
-		backgroundColor: "yellow",
-		width: "50%",
-
-	},
-	child2: {
-		width: 20,
-		backgroundColor: "blue",
-		height: 40,
-	}
-
-
-
-})
